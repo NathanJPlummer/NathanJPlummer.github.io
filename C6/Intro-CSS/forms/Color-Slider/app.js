f55f53d8d0355e2155b06bcb6a97b5d99a560534
Nathan J Plummer <DriemMastR@gmail.com>
2016-12-04 18:25:48 -0500

    Merge with private repo.  Add background color change.

--- C6/Intro-CSS/forms/Color-Slider/app.js
+++ C6/Intro-CSS/forms/Color-Slider/app.js
@@ -5,4 +5,5 @@ function customRangerColor() {
     var b = document.getElementById("blue").value;
     var g = document.getElementById("green").value;
     rangerColor.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    document.body.style.backgroundColor = "rgba(" + r + "," + g + "," + b + ", 0.05)";
 }
