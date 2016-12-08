4e025695c5e39d6e9a3f0f1a92ab261f357e9343
Nathan J Plummer <DriemMastR@gmail.com>
2016-12-08 00:08:23 -0500

    Better validation code.

--- C6/Intro-CSS/forms/Credit-Form/app.js
+++ C6/Intro-CSS/forms/Credit-Form/app.js
@@ -1,10 +1,15 @@
-var submit = document.querySelector("#submit"); // gets the submit button
-
-
-
 submit.addEventListener("click", function (event) {
     //validate Credit Card
+    validateCredit();
+    validateName();
+    validateCSV();
+});
+
+function validateCredit () {
+    //validate Credit Card Number
+    
     var creditCard = document.querySelector("#credit"); // gets the current creditCard value
+    
     if (validator.isCreditCard(creditCard.value)) {
         creditCard.setCustomValidity(""); // use setCustomValidity instead to throw pop error for input
         creditCard.style.border = "2px solid green"; // style to show invalid
@@ -12,16 +17,32 @@ submit.addEventListener("click", function (event) {
         creditCard.setCustomValidity("Invalid Credit Card Number");
         creditCard.style.border = "2px solid red";
     }
+}
 
-});
-
-/*submit.addEventListener("click", function (event) {
-
+function validateName() {
+    //validate Name
+    
     var name = document.getElementById("name");
+    
     if (validator.isName(name.value)) {
         name.setCustomValidity("");
     } else {
         name.setCustomValidity("Invalid Name");
+        name.style.border = "2px solid red";
     }
+}
 
-});*/
+function validateCSV() {
+    var csv = document.getElementById("csv");
+    
+    if (csv.value.lengh < 5) {
+        csv.setCustomValidity("CSV values never exceed 4 digits");
+        csv.style.border = "2px solid red";
+    } else if (isNaN(csv.value) === true) {
+        csv.setCustomValidity("CSV must be a number");
+        csv.style.border = "2px solid red";
+    } else {
+        csv.setCustomValidity("");
+        csv.style.border = "2px solid green";
+    }
+}
