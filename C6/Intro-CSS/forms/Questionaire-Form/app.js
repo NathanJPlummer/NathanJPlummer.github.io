8aecc823c095f66c0085a61e7b95ab8513a2fb73
Nathan J Plummer <DriemMastR@gmail.com>
2016-12-13 14:12:39 -0500

    Slightly improved validation code.

--- C6/Intro-CSS/forms/Questionaire-Form/app.js
+++ C6/Intro-CSS/forms/Questionaire-Form/app.js
@@ -1,33 +1,8 @@
-/*var form = document.querySelector("form");
-var other = document.querySelector("#other");
-
-
-
-form.addEventListener("change", function (event) {
-    otherInput = document.querySelector("#otherText");
-    
-    var radios = document.getElementsByName("choice");
-
-    //If other value is selected check for valid text input
-    if ((other.checked) && !(validator.isName(otherInput.value))) {
-        form.setCustomValidity("Please enter a valid name");
-        other.checked = false;
-    } else {
-        for (i = 0; i < radios.length; i++) {
-            if (radios[i].checked) {
-                form.setCustomValidity("");
-                alert("Thank you for your input");
-            }
-        }
-    }
-
-});*/
-
 var radios = document.getElementsByName("choice");
 
 var i; //used in loop
 for (i = 0; i < radios.length; i++) {
-    radios[i].addEventListener("click", function (event) {
+    radios[i].addEventListener("change", function (event) {
         radioResponse();
     });
 }
@@ -37,10 +12,10 @@ function radioResponse() {
     var otherInput = document.querySelector("#otherText");
     //If other radio button is selected check for valid text input
     if ((other.checked) && !(validator.isName(otherInput.value))) {
-        otherInput.setCustomValidity("Please enter a valid name");
+        event.preventDefault();
+        alert("Please enter a valid name");
         other.checked = false;
     } else {
-        otherInput.setCustomValidity("");
         alert("Thank you for your input");
     }
 }
