4b3f441dc4c0a7d4b64dcc3a4c8d4b20beb0955b
NathanJPlummer <DriemMastR@gmail.com>
2017-01-07 16:08:55 -0500

    Sync with Private repository

---
+++ C6/Intro-CSS/Group/chat.js
@@ -0,0 +1,47 @@
+var inp_text = document.getElementsByClassName("sendMessage");
+
+var inp_textarea = document.getElementById("textMessage");
+
+var panel_body = document.getElementsByClassName("panelBody");
+
+var chat_box = document.getElementById("chatbox");
+
+inp_text[0].addEventListener("click", function (e) {
+    createMessage();
+});
+
+function createMessage() {
+    var commentBox = document.createElement("div");
+    var image = document.createElement("img");
+    var comment = document.createElement("div");
+    var header = document.createElement("div");
+    var name = document.createElement("span");
+    var time = document.createElement("span");
+    var text = document.createElement("p");
+
+    text.setAttribute("class", "message");
+    text.appendChild(document.createTextNode(inp_textarea.value));
+
+    time.setAttribute("class", "time");
+    time.appendChild(document.createTextNode("right now"));
+
+    name.setAttribute("class", "userName");
+    name.appendChild(document.createTextNode(chat_box.dataset.username));
+
+    header.setAttribute("class", "header");
+    header.appendChild(time);
+    header.appendChild(name);
+
+    comment.setAttribute("class", "comment nocolor");
+    comment.appendChild(header);
+    comment.appendChild(text);
+
+    image.setAttribute("class", "user");
+    image.setAttribute("src", "images/userimage.jpg");
+
+    commentBox.setAttribute("class", "commentBox commentBoxRight");
+    commentBox.appendChild(image);
+    commentBox.appendChild(comment);
+
+    panel_body[0].appendChild(commentBox);
+}
