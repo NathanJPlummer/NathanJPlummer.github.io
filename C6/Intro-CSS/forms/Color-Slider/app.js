579963c83cc36c72507ff6737259d528e5a86cd1
Nathan J Plummer <DriemMastR@gmail.com>
2017-07-29 22:08:14 -0400

    Complete GHPages restructure that places portfolio at front.

--- C6/Intro-CSS/forms/Color-Slider/app.js
+++
@@ -1,9 +0,0 @@
-function customRangerColor() {
-    var rangerColor = document.getElementById("custom");
-
-    var r = document.getElementById("red").value;
-    var b = document.getElementById("blue").value;
-    var g = document.getElementById("green").value;
-    rangerColor.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
-    document.body.style.backgroundColor = "rgba(" + r + "," + g + "," + b + ", 0.05)";
-}
