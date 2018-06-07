c40f487cdf9f628d2f72d96262784491723c115e
NathanJPlummer <DriemMastR@gmail.com>
2018-06-06 21:59:01 -0400

    Remove files now only needed in dev branch.

--- JavaScript/onload.js
+++
@@ -1,8 +0,0 @@
-window.onload = pre_loader;
-
-
-
-function pre_loader() {
-
-  document.getElementById('home-nav-a-home').click();
-}
--- JavaScript/portfolio-utilities.js
+++
@@ -1,7 +0,0 @@
-//reusuable functions written for this Portfolio
-
-//get index of child element from parent
-function getIndex(element) {
-  for (var i=0; (element=element.previousElementSibling); i++);
-  return i;
-}
