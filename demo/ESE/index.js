795a8d0decd9663aac7721edd0fc1ede62a8334f
root <root@SPH-ESMJ033LJD.ad.unc.edu>
2019-02-20 15:06:24 -0500

    Update ESE demo to current codebase.

--- demo/ESE/index.js
+++ demo/ESE/index.js
@@ -5,6 +5,12 @@ selectbox.addEventListener('change', function(){
     if (selectbox.value === "1") {
         iframe.src = "https://unc.az1.qualtrics.com/jfe/form/SV_cI8bJorTom8HkeF"; 
     } else {
-        iframe.src = "https://unc.az1.qualtrics.com/jfe/form/SV_eXKingfdHuZGcrb";
+        iframe.src = "https://unc.az1.qualtrics.com/jfe/form/SV_3lYbbJnlqXm7pvn";
+    }
+});
+
+window.addEventListener("load", function(){
+    if (window.screen.width < 595) {
+        iframe.width = "250px";
     }
 });
